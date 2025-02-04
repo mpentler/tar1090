c1b0281bfa96e1484360379ef08c126a3ac547cb
Mark Pentler <27921983+mpentler@users.noreply.github.com>
2025-02-04 11:12:53 +0000

    Update install.sh
    updating for use with multiple instance tar1090 config

--- install.sh
+++ install.sh
@@ -498,4 +498,4 @@ else
 fi
 
 # OARC changes
-cp -r /home/mark/airline-logo-scraper/avcodes_banners/ /usr/local/share/tar1090/html
+cp -r /home/mark/airline-logo-scraper/avcodes_banners/ /usr/local/share/tar1090/html-webroot
